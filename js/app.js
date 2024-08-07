f1a6fd416b7eeea9cfce8e68463ad407dda0e3c8
nileshnaik95 <nileshnaik.frontend@gmail.com>
2024-08-07 23:27:08 +0530

    Basic Setup

--- js/app.js
+++ js/app.js
@@ -1,5 +1,43 @@
-if('serviceWorker' in navigator){
-  navigator.serviceWorker.register('/sw.js')
-    .then(reg => console.log('service worker registered'))
-    .catch(err => console.log('service worker not registered', err));
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('./sw.js')
+    .then((reg) => console.log('service worker registered'))
+    .catch((err) => console.log('service worker not registered', err));
 }
+
+// Request permission and get token
+document
+  .getElementById('enable-notifications')
+  .addEventListener('click', function () {
+    messaging
+      .requestPermission()
+      .then(function () {
+        console.log('Notification permission granted.');
+        return messaging.getToken();
+      })
+      .then(function (token) {
+        console.log('Token:', token);
+      })
+      .catch(function (err) {
+        console.log('Unable to get permission to notify.', err);
+      });
+  });
+
+// Handle incoming messages
+messaging.onMessage(function (payload) {
+  console.log('Message received. ', payload);
+  // Customize notification here
+  var notificationTitle = payload.notification.title;
+  var notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.icon
+  };
+
+  if (Notification.permission === 'granted') {
+    var notification = new Notification(notificationTitle, notificationOptions);
+    notification.onclick = function (event) {
+      event.preventDefault();
+      window.open(payload.notification.click_action, '_blank');
+    };
+  }
+});
--- sw.js
+++ sw.js
@@ -1,3 +1,30 @@
+importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-messaging.js');
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAiLboPaKcUEQsJPpxcgbV-5eArOBbRvAY',
+  authDomain: 'foodcart-9d5b7.firebaseapp.com',
+  projectId: 'foodcart-9d5b7',
+  storageBucket: 'foodcart-9d5b7.appspot.com',
+  messagingSenderId: '173849797247',
+  appId: '1:173849797247:web:a82e3f7ae368864c6d954a',
+  measurementId: 'G-ZBR1C73CJW'
+};
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function (payload) {
+  console.log('Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: './img/firebase-logo.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
 const staticCacheName = 'site-static-v4';
 const dynamicCacheName = 'site-dynamic-v4';
 const assets = [
